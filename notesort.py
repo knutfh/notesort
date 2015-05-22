853fbc1fb80fe7bd8d03ab36dc8c919061088418
Knut Flage Henriksen <post@flaksen.com>
2015-05-22 22:13:42 +0200

    Working rename for prefix and base

---
+++ notesort.py
@@ -0,0 +1,22 @@
+import os
+
+mapping = {}
+folder = '/Users/knut/dev/python/notesort/test/'
+
+with open('Instruments.txt') as mapping_file:
+    for line in mapping_file:
+        prefix, body = line.split()
+        mapping[prefix] = body
+
+for filename in os.listdir(folder):
+    prefix, extension = os.path.splitext(filename)
+    # print prefix, extension
+    if not extension.endswith('.pdf'):
+        continue
+    if prefix in mapping:
+        newFileName = ''.join(prefix + '_' + mapping[prefix] + extension)
+        oldPath = os.path.join(folder, filename)
+        newPath = os.path.join(folder, newFileName)
+        # print ''.join("Renaming " + filename + " --> " + newFileName)
+        os.rename(oldPath, newPath)
+        print 'Renamed', filename, '-->', newFileName
