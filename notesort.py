7c9d4823fedb0d8bf914e922f77de2144221ecbc
Knut Flage Henriksen <post@flaksen.com>
2015-05-22 22:35:31 +0200

    Removed unneded files from versioning

--- notesort.py
+++ notesort.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
-mapping = {}
 folder = os.path.abspath(sys.argv[1])
 
+mapping = {}
+
+# populate dictionary with instrument names
 with open('Instruments.txt') as mapping_file:
     for line in mapping_file:
         prefix, body = line.split()
@@ -11,13 +13,12 @@ with open('Instruments.txt') as mapping_file:
 
 for filename in os.listdir(folder):
     prefix, extension = os.path.splitext(filename)
-    # print prefix, extension
+    # only rename PDF files
     if not extension.endswith('.pdf'):
         continue
     if prefix in mapping:
         newFileName = ''.join(prefix + '_' + mapping[prefix] + extension)
         oldPath = os.path.join(folder, filename)
         newPath = os.path.join(folder, newFileName)
-        # print ''.join("Renaming " + filename + " --> " + newFileName)
         os.rename(oldPath, newPath)
         print 'Renamed', filename, '-->', newFileName
