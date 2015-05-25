3e47eda8193101bd4892ee48e96a86b48c2c763c
Knut Flage Henriksen <post@flaksen.com>
2015-05-25 12:31:38 +0200

    Added handling of multiple parts per instrument

--- notesort.py
+++ notesort.py
@@ -20,15 +20,29 @@ def rename_files_with_instrument_name(instrument_map):
         root, extension = os.path.splitext(filename)
         if not extension.endswith('.pdf'):
             continue
-        if root in instrument_map:
-            new_file_name = '{prefix}_{instrument_name}{extension}'.format(
-                                prefix=root,
-                                instrument_name=instrument_map[root],
-                                extension=extension)
-            old_path = os.path.join(FOLDER, filename)
-            new_path = os.path.join(FOLDER, new_file_name)
-            os.rename(old_path, new_path)
-            print 'Renamed', filename, '-->', new_file_name
+        try:
+            if root[2]:
+                part_number = root[3]
+                instrument_number = root[:2]
+                if instrument_number in instrument_map:
+                    new_file_name = '{prefix}_'\
+                                    '{instrument_name}'\
+                                    '{part_number}'\
+                                    '{extension}'.format(
+                                        prefix=root[:2],
+                                        instrument_name=instrument_map[root[:2]],
+                                        part_number=root[3],
+                                        extension=extension)
+        except:
+            if root in instrument_map:
+                new_file_name = '{prefix}_{instrument_name}{extension}'.format(
+                                    prefix=root,
+                                    instrument_name=instrument_map[root],
+                                    extension=extension)
+        old_path = os.path.join(FOLDER, filename)
+        new_path = os.path.join(FOLDER, new_file_name)
+        os.rename(old_path, new_path)
+        print 'Renamed', filename, '-->', new_file_name
 
 if __name__ == '__main__':
     instrument_map = create_instrument_map()
