7370d5d9b59b9c7e775d17d13f4da271dc8438dc
Knut Flage Henriksen <post@flaksen.com>
2015-05-25 16:46:16 +0200

    Passing tests

--- test_notesort.py
+++ test_notesort.py
@@ -1,6 +1,16 @@
 import unittest
+from notesort import create_instrument_map
 
 
 class TestNoteSort(unittest.TestCase):
 
-    def test_
+    def test_create_instrument_map_creates_soprano_cornet(self):
+        instrument_map = create_instrument_map()
+        self.assertEqual(instrument_map['01'], 'SopranoCn')
+
+    def test_create_instrument_map_soprano_not_solo(self):
+        instrument_map = create_instrument_map()
+        self.assertNotEqual(instrument_map['01'], 'SoloCn')
+
+if __name__ == '__main__':
+    unittest.main()
